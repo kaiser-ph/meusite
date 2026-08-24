d659c0a5907e1bb38c37147c942318dbed8b9a0c
kaiser-ph <kaiser.philipp@escola.pr.gov.br>
2026-08-24 08:17:17 -0300

    Create script.java

---
+++ script.java
@@ -0,0 +1,4 @@
+document.getElementById('meuBotao').addEventListener('click', function() {
+  const mensagem = document.getElementById('mensagem');
+  mensagem.textContent = 'Olá! Você interagiu com o site usando JavaScript!';
+});
